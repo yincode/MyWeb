a6d48112bd8932dcfe5f9510c5588be6549e3012
yin <15733253292@163.com>
2016-10-05 11:25:40 +0800

    1
    1

--- C   demo/5.   canvas绘制数字钟/countdown.js
+++ C   demo/5.   canvas绘制数字钟/countdown.js
@@ -4,7 +4,7 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
-const endTime = new Date(2016,7,18,18,47,52);
+const endTime = new Date(2016,10,3,18,47,52);
 var curShowTimeSeconds = 0
 
 var balls = [];
